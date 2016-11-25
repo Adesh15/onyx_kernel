6bb22897f73706016bc4758a5eae185fead806f8
Adesh15 <adesikha15@gmail.com>
2016-11-25 20:39:53 +0530

    source: Added Global Build Script

---
+++ build.sh
@@ -0,0 +1,66 @@
+#For Time Calculation
+BUILD_START=$(date +"%s")
+
+# Housekeeping
+blue='\033[0;34m'
+cyan='\033[0;36m'
+green='\033[1;32m'
+red='\033[0;31m'
+nocol='\033[0m'
+
+# 
+# Configure following according to your system
+# 
+
+# Directories
+KERNEL_DIR=$PWD
+KERN_IMG=$KERNEL_DIR/arch/arm/boot/zImage-dtb
+OUT_DIR=$KERNEL_DIR/zipping/onyx
+STAR_VERSION="alpha-0.1"
+
+# Device Spceifics
+export ARCH=arm
+export CROSS_COMPILE="/home/adesh15/android/ndk/android-ndk-r13b/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-"
+export KBUILD_BUILD_USER="hatti"
+export KBUILD_BUILD_HOST="ghoda"
+
+########################
+## Start Build Script ##
+########################
+
+# Remove Last builds
+rm -rf $OUT_DIR/*.zip
+rm -rf $OUT_DIR/zImage
+
+compile_kernel ()
+{
+echo -e "$green ********************************************************************************************** $nocol"
+echo "                    "
+echo "                                   Compiling Star-Kernel                    "
+echo "                    "
+echo -e "$green ********************************************************************************************** $nocol"
+#make clean && make mrproper
+make slim_onyx_defconfig
+make -j32
+if ! [ -a $KERN_IMG ];
+then
+echo -e "$red Kernel Compilation failed! Fix the errors! $nocol"
+exit 1
+fi
+zipping
+}
+
+
+zipping() {
+
+# make new zip
+cp $KERN_IMG $OUT_DIR/zImage
+cd $OUT_DIR
+zip -r StarKernel-onyx-$STAR_VERSION-$(date +"%Y%m%d")-$(date +"%H%M%S").zip *
+}
+compile_kernel
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "$blue Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
+echo -e "$red zImage size (bytes): $(stat -c%s $KERN_IMG) $nocol"
+
